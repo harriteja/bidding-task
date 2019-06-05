c87ffb0ebfbe36570de64045fe9d4690a42fbecf
HariTeja <hariteja@fptechscience.com>
2019-06-05 08:51:24 +0530

    cors issue resolved

---
+++ src/main/java/com/example/demo/config/ApplicationConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        List<String> values = new ArrayList<String>();
+        values.add("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(values);
+        config.setAllowedHeaders(values);
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
